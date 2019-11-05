cd0236e725c969af200efc02a4cba6d24bfb979c
Abel Lazaro <abel@iosappstudio.com>
2019-11-04 18:03:13 -0600

    mejoras visuales y compatibilidad con iOS9 en adelante

--- MetalSlug/ViewController.swift
+++ MetalSlug/ViewController.swift
@@ -13,68 +13,64 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    //MARK: - variables, constantes y objetos
     var track: String? = nil
     var audioPlayer: AVAudioPlayer?
 
+    //MARK: - ciclo de vida
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    //MARK: - acction de botones
     
     @IBAction func heavyButton(_ sender: Any) {
         track = "H"
-        print("heavy machine gun \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func rocketButton(_ sender: Any) {
         track = "R"
-        print("Rocket launcher \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func flameButton(_ sender: Any) {
         track = "F"
-        print("flame shot \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func laserButton(_ sender: Any) {
         track = "L"
-        print("laser gun \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func shotButton(_ sender: Any) {
         track = "S"
-        print("shot gun \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func enemyButton(_ sender: Any) {
         track = "C"
-        print("enemy chacer \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func ironButton(_ sender: Any) {
         track = "I"
-        print("iron lizard \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func dropButton(_ sender: Any) {
         track = "D"
-        print("drop shot \(track!)")
         reproducirAudio(audio: track!)
     }
     
     @IBAction func granadeButton(_ sender: Any) {
         track = "G"
-        print("super granade \(track!)")
         reproducirAudio(audio: track!)
     }
     
+    //MARK: - funcion reproductor de sonido
     func reproducirAudio(audio: String) {
         let path = Bundle.main.path(forResource: "\(audio).mp3", ofType:nil)!
         let url = URL(fileURLWithPath: path)
@@ -83,7 +79,7 @@ class ViewController: UIViewController {
             audioPlayer = try AVAudioPlayer(contentsOf: url)
             audioPlayer?.play()
         } catch {
-            // couldn't load file :(
+            // no se pudo cargar el archivo :(
         }
     }
 }
